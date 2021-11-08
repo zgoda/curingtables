b1f48b0b42796840b5f213d8a0139c942bcfea38
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-11-08 16:01:34 +0100

    Module declarations

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,9 @@
-declare module '*.css';
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
