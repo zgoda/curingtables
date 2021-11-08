0104e4eb9edaa4cde2a5f6740994746f5efecad7
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-11-08 15:57:03 +0100

    Local scope for styles

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,6 +1,8 @@
+import style from './calculator.module.css';
+
 export function Calculator() {
   return (
-    <section class="section-wrapper">
+    <section class={style.sectionWrapper}>
       <div>Kalkulator</div>
       <div>Pomoc</div>
     </section>
